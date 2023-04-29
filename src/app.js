5d9f91012c66da19cef5cd0d6eede1ba6b2d2656
nasim67reja <nasimreja67@gmail.com>
2023-04-29 18:09:28 +0600

    new cors origin added

--- src/app.js
+++ src/app.js
@@ -40,6 +40,7 @@ app.use(
       'https://adminswopnew.netlify.app',
       'https://swopme.co',
       'https://swop-dashboard.vercel.app',
+      'ttps://test-frontend-eta-two.vercel.app',
     ],
     credentials: true,
   })
